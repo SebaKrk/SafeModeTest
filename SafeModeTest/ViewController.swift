4c8ac0efccefb11622d8b39df3db27b521751a51
SebaKrk <s.sciuba@gmail.com>
2023-05-04 07:40:28 +0200

    Initial Commit

---
+++ SafeModeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeModeTest
+//
+//  Created by Sebastian Sciuba on 04/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
